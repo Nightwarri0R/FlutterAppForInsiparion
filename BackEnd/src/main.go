31148789f629437a006bf86d162ba81fdf20be00
Aylin <gamena750@abv.bg>
2023-02-23 20:22:57 +0000

    Need to figure out why the json is returned empty

--- BackEnd/src/main.go
+++ BackEnd/src/main.go
@@ -4,23 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"log"
+	"encoding/json"
 	"github.com/gorilla/mux"
 )
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("HomePage")
-}
-func handleRoutes(){
-	router := mux.NewRouter()
-	router.HandleFunc("/", homePage).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8090", router))
-}
-
-func main() {
-	handleRoutes()
-	allQuotes()
-	fmt.Println(quotes)
-}
-
 type Quotes struct {
 	ID     string `json:"id"`
 	Quote  string `json:"quote"`
@@ -37,4 +23,48 @@ func allQuotes() {
 		Author: "John Doe",
 		Date:   "1979-05-27"}
 	quotes = append(quotes, quote)
+
+	quote1 := Quotes{
+		ID:     "322",
+		Quote:  "I am a quote",
+		Author: "John Doe",
+		Date:   "1979-05-27"}
+	quotes = append(quotes, quote1)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HomePage")
+}
+func getQuote(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "appication/")
+	json.NewEncoder(w).Encode(quotes)
+}
+func getQuotes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "appication/")
+	json.NewEncoder(w).Encode(quotes)
+}
+func addQuote(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HomePage")
+}
+func deleteQuote(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HomePage")
+}
+func editQuote(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HomePage")
+}
+func handleRoutes(){
+	router := mux.NewRouter()
+	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/getquotes", getQuotes).Methods("GET")
+	router.HandleFunc("/getquote", getQuote).Methods("GET")
+	router.HandleFunc("/addquote", addQuote).Methods("POST")
+	router.HandleFunc("/deletequote{id}", deleteQuote).Methods("DELETE")
+	router.HandleFunc("/editquote{id}", editQuote).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8090", router))
+}
+
+func main() {
+	handleRoutes()
+	allQuotes()
+	fmt.Println(quotes)
 }
